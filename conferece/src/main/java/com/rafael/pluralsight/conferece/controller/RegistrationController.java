8f49484dca48001a7d2e96a2a96c21b76174952c
Rafael Louro <rlourox@gmail.com>
2020-03-18 18:34:23 -0300

    ViewResolver e arquivos estaticos

--- conferece/src/main/java/com/rafael/pluralsight/conferece/controller/RegistrationController.java
+++ conferece/src/main/java/com/rafael/pluralsight/conferece/controller/RegistrationController.java
@@ -18,7 +18,7 @@ public class RegistrationController {
 
     @PostMapping("registration")
     public String addRegistration(@ModelAttribute("registration") Registration registration) {
-        System.out.println("Registration" + registration.getName());
+        System.out.println("Registration " + registration.getName());
         return "registration";
     }
 }
