9723c4aa6044125b920b7686371075736c7f6444
Rafael Louro <rlourox@gmail.com>
2020-03-20 17:24:26 -0300

    Pagina do thymeleaf funcionando

--- conference-thymeleaf/src/main/java/com/rafael/pluralsight/conference/ConferenceConfig.java
+++ conference-thymeleaf/src/main/java/com/rafael/pluralsight/conference/ConferenceConfig.java
@@ -1,5 +1,7 @@
 package com.rafael.pluralsight.conference;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
@@ -10,11 +12,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import java.util.Locale;
 
 @Configuration
 public class ConferenceConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -46,7 +54,32 @@ public class ConferenceConfig implements WebMvcConfigurer {
         InternalResourceViewResolver bean = new InternalResourceViewResolver();
         bean.setPrefix("/WEB-INF/jsp/");    //spring.mvc.view.prefix=/WEB-INF/jsp/
         bean.setSuffix(".jsp");             //spring.mvc.view.suffix=.jsp
-        bean.setOrder(0);
+        bean.setOrder(1);
         return bean;
+    }
+
+    @Bean
+    public ViewResolver thymeleafViewResolver() {
+        ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+        thymeleafViewResolver.setTemplateEngine(templateEngine());
+        thymeleafViewResolver.setOrder(0);
+        return thymeleafViewResolver;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
+        springResourceTemplateResolver.setApplicationContext(applicationContext);
+        springResourceTemplateResolver.setPrefix("/WEB-INF/views/");
+        springResourceTemplateResolver.setSuffix(".html");
+        return springResourceTemplateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.setTemplateResolver(templateResolver());
+        springTemplateEngine.setEnableSpringELCompiler(true);
+        return springTemplateEngine;
     }
 }
--- conference-thymeleaf/src/main/java/com/rafael/pluralsight/conference/controller/GreetingController.java
+++ conference-thymeleaf/src/main/java/com/rafael/pluralsight/conference/controller/GreetingController.java
@@ -14,4 +14,10 @@ public class GreetingController {
         return "greeting";
     }
 
+    @GetMapping("thyme")
+    public String thyme(Map<String, Object> model) {
+        model.put("message", "Mensagem do backend - Thymeleaf!");
+        return "thyme";
+    }
+
 }
